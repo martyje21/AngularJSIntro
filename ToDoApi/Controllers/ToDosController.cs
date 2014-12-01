b00bf5dae5d37099758b1e62737a510fb9d944a3
Marty Elvidge <martyje@gmail.com>
2014-11-30 19:10:50 -0500

    Added ToDoApi application

---
+++ ToDoApi/Controllers/ToDosController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    public class ToDosController : ApiController
+    {
+        // GET: api/ToDos
+        public IEnumerable<ToDo> Get()
+        {
+            ToDo td1 = new ToDo() { dueDate = new DateTime(2014, 12, 5), Id = "12345680", Title = "Do This" };
+            ToDo td2 = new ToDo() { dueDate = new DateTime(2014, 12, 31), Id = "09876543", Title = "Then that" };
+            return new ToDo[] { td1, td2 };
+        }
+
+        // GET: api/ToDos/5
+        public ToDo Get(string id)
+        {
+            return new ToDo();
+        }
+
+        // POST: api/ToDos
+        public HttpResponseMessage Post([FromBody]ToDo value)
+        {
+            HttpResponseMessage retval = new HttpResponseMessage(HttpStatusCode.OK);
+
+            return retval;
+        }
+
+        // PUT: api/ToDos/5
+        public HttpResponseMessage Put(string id, [FromBody]ToDo value)
+        {
+            HttpResponseMessage retval = new HttpResponseMessage(HttpStatusCode.OK);
+
+            return retval;
+        }
+
+        // DELETE: api/ToDos/5
+        public HttpResponseMessage Delete(string id)
+        {
+            HttpResponseMessage retval = new HttpResponseMessage(HttpStatusCode.OK);
+
+            return retval;
+        }
+    }
+}
---
+++ ToDoApi/Models/ToDo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ToDoApi.Models
+{
+    public class ToDo
+    {
+        public string Id { get; set; }
+
+        public string Title { get; set; }
+
+        public DateTime dueDate { get; set; }
+
+    }
+}
